26ab5c8c77fad2f44aa72eacbd39ee0e6d41157c
Pedro <pg2928@gmail.com>
2023-05-22 23:36:32 -0300

    estilizando o form_tarefa

--- app/models.py
+++ app/models.py
@@ -1,14 +1,11 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
-
-
 class Tarefa(models.Model):
     PRIORIDADE_CHOICES = [
-        ("A", "Alta"),
-        ("N", "Normal"),
-        ("B", "Baixa"),
+        ("A", "ALTA"),
+        ("N", "NORMAL"),
+        ("B", "BAIXA"),
     ]
 
     titulo = models.CharField(max_length=30, null=False, blank=False)
@@ -17,3 +14,9 @@ class Tarefa(models.Model):
     prioridade = models.CharField(max_length=1, choices=PRIORIDADE_CHOICES, null=False, blank=False)
 
     usuario = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "Tarefas"
+
+    def __str__(self):
+        return self.titulo
--- gerenciador_tarefas/settings.py
+++ gerenciador_tarefas/settings.py
@@ -124,9 +124,9 @@ LOGIN_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app/templates/static')
+    os.path.join(BASE_DIR, 'static/')
 ]
 
